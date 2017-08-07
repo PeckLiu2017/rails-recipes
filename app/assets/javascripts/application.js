c0e269296c6f4ae8be747a31c7cedf32c96cb150
peck liu <xiaoyang20170204@gmail.com>
2017-08-07 23:42:38 +0800

    rails tools 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require select2
+//= require nested_form_fields
 //= require_tree .
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,10 @@ class Event < ApplicationRecord
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
   belongs_to :category, :optional => true
-  
+  has_many :tickets, :dependent => :destroy, :inverse_of  => :event
+  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
+
   def to_param
     # "#{self.id}-#{self.name}"
     self.friendly_id
