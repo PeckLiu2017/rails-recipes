afbcb780c513ba777d847affa200949c4be7a8cc
peck liu <xiaoyang20170204@gmail.com>
2017-08-07 13:19:30 +0800

    languge added & time_zone is to be finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-
+  resource :user
+  
   namespace :admin do
     root "events#index"
     resources :events
   end
 
   root "events#index"
-
+  get "/faq" => "pages#faq"
 end
