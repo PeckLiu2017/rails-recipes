37b9ad916213c0d77b2547687580e465206de923
peck liu <xiaoyang20170204@gmail.com>
2017-08-07 15:04:13 +0800

    fixed selected option function

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ApplicationRecord
-
+  STATUS = ["draft", "public", "private"]
+  validates_inclusion_of :status, :in => STATUS
   validates_presence_of :name, :friendly_id
   before_validation :generate_friendly_id, :on => :create
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
-  
+
   def to_param
     # "#{self.id}-#{self.name}"
     self.friendly_id
