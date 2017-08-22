c031d9595a96c21117d66876295a50ca9221bb9c
peck liu <xiaoyang20170204@gmail.com>
2017-08-22 13:53:45 +0800

    analyse totle price

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -25,7 +25,17 @@ class Admin::EventsController < AdminController
          backgroundColor: colors,
          borderWidth: 1
        }]
-     }
+    }
+
+    @data2 = {
+      labels: ticket_names,
+      datasets: [{
+        label: '# of Amount',
+        data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+        backgroundColor: colors,
+        borderWidth: 1
+      }]
+    }
   end
 
   def new
