97b0da767332ab297adaeeff0bc4b12d9a0e90f6
peck liu <xiaoyang20170204@gmail.com>
2017-08-24 16:54:37 +0800

    send notification after registration

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,21 +1,21 @@
 class AdminController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :require_admin!
+  # before_action :require_admin!
   before_action :require_editor!
   layout "admin"
 
   protected
     def require_editor!
       unless current_user.is_editor?
-        flash[:alert] = "权限不足"
+        flash[:alert] = "权限不足1"
         redirect_to root_path
       end
     end
 
     def require_admin!
       unless current_user.is_admin?
-        flash[:alert] = "权限不足"
+        flash[:alert] = "权限不足3"
         redirect_to root_path
       end
     end
